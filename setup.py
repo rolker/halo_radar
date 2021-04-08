35c8b3745b0ca6fd4445204587f2b9f9ee37f656
Roland Arsenault <roland@ccom.unh.edu>
2021-04-08 09:31:56 -0400

    Remove python implementation

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-d = generate_distutils_setup(
-    packages=['halo_radar'],
-    package_dir={'': 'src'}
-)
-
-setup(**d)
